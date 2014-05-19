858a49c43a0b99ec3645b7e985ba480dbb5ba924
Wei-En Wong <weienwong.93@gmail.com>
2014-05-18 22:42:42 -0400

    Changed index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var express = require('express')
 var app = express();
+var fs = require('fs');
+
+var buff = fs.readFileSync("index.html");
+var str = buff.toString();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  response.send(str)
 })
 
 app.listen(app.get('port'), function() {
