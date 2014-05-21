12621bb885be1106daaff5b31cce63af30a57eaa
Wei-En Wong <weienwong.93@gmail.com>
2014-05-21 11:24:43 -0400

    Edited product desc in index.html

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 var buff = fs.readFileSync("index.html");
 var str = buff.toString();
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
